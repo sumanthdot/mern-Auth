e094d081ef3c894dfe5c63c84ef9a9c462690d53
sumanthdot <sumanthdotg@gmail.com>
2024-12-31 15:34:35 +0530

    inde

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import path from 'path';
-import {connectDB} from './db/connectDB.js'
+import connectDB from './db/connectDB.js'
 // import { connectDb } from './db/connectDb.js';//
 import authRoutes from './routes/auth.route.js'
 
